f76b5ba1c5bb87c9b4044ca54f5a378f4a05a931
gavindu98 <rathnayakagavindu98@gmail.com>
2022-01-09 20:35:47 -0800

    laracel_first

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            Home page
+            <p>Add something to the home page</p>
+        </div>
+    </div>
+@endsection
